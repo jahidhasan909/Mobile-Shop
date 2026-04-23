8713b88b0b05142bccc929e50110ccc52950fd51
Jahid hasan <jahidhasan@Jahids-MacBook-Air.local>
2026-04-23 13:17:38 +0600

    phone section all phone data fetch add load and show ui

---
+++ src/Components/AllCards/MotorolaCard.jsx
@@ -0,0 +1,23 @@
+import { Button, Card } from '@heroui/react';
+
+const MotorolaCard = ({motorolas}) => {
+    return (
+        <div>
+            <Card className="bg-white rounded-md py-6 hover:shadow-md hover:shadow-neutral-200" variant="tertiary">
+                <Card.Header className=' items-center  justify-center space-y-2'>
+                    <img className='h-[200px]' src={motorolas.img} alt="" />
+                    <Card.Title>{motorolas.model}</Card.Title>
+                    <Card.Title className='font-bold'>{motorolas.price}</Card.Title>
+                </Card.Header>
+                <Card.Content >
+                    <div className='flex gap-2 items-center justify-center'>
+                        <Button className=' rounded-md w-full' variant='outline'>View</Button>
+                        <Button className='rounded-md w-full bg-black text-white' variant='outline'>Add To Cart !</Button>
+                    </div>
+                </Card.Content>
+            </Card>
+        </div>
+    );
+};
+
+export default MotorolaCard;
---
+++ src/Components/DropDownInner/RedmiDropDown.jsx
@@ -0,0 +1,25 @@
+import { ChevronDown } from "@gravity-ui/icons";
+import { Button, Dropdown, Label } from "@heroui/react";
+const RedmiDropDown = () => {
+    return (
+        <div>
+            <Dropdown className="">
+                <Button aria-label="Menu" variant="normal" className='bg-neutral-200 rounded-md p-7'>
+                    Filter by <ChevronDown></ChevronDown>
+                </Button>
+                <Dropdown.Popover>
+                    <Dropdown.Menu onAction={(key) => console.log(`Selected: ${key}`)}>
+                        <Dropdown.Item id="new-file" textValue="New file">
+                            <Label>Low - High</Label>
+                        </Dropdown.Item>
+                        <Dropdown.Item id="copy-link" textValue="Copy link">
+                            <Label>High - Low</Label>
+                        </Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown.Popover>
+            </Dropdown>
+        </div>
+    );
+};
+
+export default RedmiDropDown;
