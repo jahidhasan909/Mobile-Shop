8b35c4ac6b4bb294069524aa08552400abed89e3
Jahid hasan <jahidhasan@Jahids-MacBook-Air.local>
2026-04-23 12:53:38 +0600

    huawei and samsung data fetch and show ui compeletd

--- src/app/phone/huawei/page.jsx
+++ src/app/phone/huawei/page.jsx
@@ -1,39 +1,37 @@
-import CardIphone from "@/Components/AllCards/CardIphone";
-import InnerDropDown from "@/Components/DropDownInner/InnerDropDown";
+import HuaweiCard from "@/Components/AllCards/HuaweiCard";
+import HuaweiDropDown from "@/Components/DropDownInner/HuaweiDropDown";
 import { House } from "@gravity-ui/icons";
 import { Button } from "@heroui/react";
 
 
-
-
-const IphonePage = async () => {
+const HuaweiPage = async () => {
     const data = await fetch("http://localhost:3000/data.json")
     const res = await data.json()
-    const iphone = res.phone.iphone
+    const huawei = res.phone.huawei
 
     return (
-        <div className=" container mx-auto my-10">
+        <div className="container mx-auto my-10">
             <p className="py-3 flex items-center gap-1"> <House></House> / Products / Categories / phones</p>
             <div className="bg-white p-5 rounded-md my-4">
-                <Button className="bg-black text-white" variant="outline">Apple</Button>
+                <Button className="bg-black text-white" variant="outline">Huawei</Button>
             </div>
             <div className="flex justify-between items-center gap-3">
                 <div className="bg-neutral-200 w-full p-4 rounded-md">
                     <h3>Products of phones</h3>
                 </div>
 
-                <InnerDropDown></InnerDropDown>
+                <HuaweiDropDown></HuaweiDropDown>
 
             </div>
 
-            <div className="grid grid-cols-4 gap-4 my-5">
-                {iphone.map(iphones => <CardIphone key={iphones.id} iphones={iphones}></CardIphone>)}
+
+            <div className="grid grid-cols-4 gap-3 my-5">
+                {
+                    huawei.map(huaweis => <HuaweiCard key={huaweis.id} huaweis={huaweis}></HuaweiCard>)
+                }
             </div>
-
-
-
         </div>
     );
 };
 
-export default IphonePage;
+export default HuaweiPage;
---
+++ src/app/phone/samsung/page.jsx
@@ -0,0 +1,37 @@
+import SamsungCard from '@/Components/AllCards/SamsungCard';
+import SamsungDropDown from '@/Components/DropDownInner/SamsungDropDown';
+import { House } from '@gravity-ui/icons';
+import { Button } from '@heroui/react';
+import React from 'react';
+
+const SamsungPage = async () => {
+
+    const data = await fetch("http://localhost:3000/data.json")
+    const res = await data.json()
+    const samsung = res.phone.samsung
+
+    return (
+        <div className=" container mx-auto my-10">
+            <p className="py-3 flex items-center gap-1"> <House></House> / Products / Categories / phones</p>
+            <div className="bg-white p-5 rounded-md my-4">
+                <Button className="bg-black text-white" variant="outline">Samsung</Button>
+            </div>
+            <div className="flex justify-between items-center gap-3">
+                <div className="bg-neutral-200 w-full p-4 rounded-md">
+                    <h3>Products of phones</h3>
+                </div>
+
+                <SamsungDropDown></SamsungDropDown>
+
+            </div>
+            <div className=' grid grid-cols-4 my-5 gap-4'>
+
+                {
+                    samsung.map(samsungs => <SamsungCard key={samsungs.id} samsungs={samsungs}></SamsungCard>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default SamsungPage;
